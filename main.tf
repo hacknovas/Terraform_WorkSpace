7cf66dabf27cf88f29526f0c06abb934e05b6dcb
hacknovas <creatives.doni@gmail.com>
2024-01-19 23:26:51 +0530

    Updates On RP

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-provider "aws" {
-  region = "ap-south-1"
+resource "aws_instance" "test_instance" {
+  ami = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+  provider = aws.reg1
 }
 
-# resource "aws_instance" "test_instance" {
-#   ami = "ami-03f4878755434977f"
-#   instance_type = "t2.micro"
-# }
-
-# resource "aws_s3_bucket" "test_bucket" {
-#   bucket = "test-my-bucket-mahi"
-#   object_lock_enabled = true 
-# }
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "work-withpdbucket-test"
+  object_lock_enabled = true 
+  provider = aws.reg2
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  alias = "reg1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "reg2"
+  region = "us-east-1"
+}
