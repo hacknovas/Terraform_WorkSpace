7d4f5e852df0e1755dc3bab8519b5525c7cfb13e
hacknovas <creatives.doni@gmail.com>
2024-01-19 21:01:51 +0530

    CreatedTerraform

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-provider "" {
-  
+provider "aws" {
+  region = "ap-south-1"
 }
 
-resource "" "" {
-  
+resource "aws_instance" "test_example" {
+  ami = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
 }
