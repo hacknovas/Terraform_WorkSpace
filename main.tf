234de041dad3d2397b9f48c4c042d4f9dd8e8397
hacknovas <creatives.doni@gmail.com>
2024-01-19 21:50:55 +0530

    TerraformBasics

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "test_example" {
+resource "aws_instance" "test_instance" {
   ami = "ami-03f4878755434977f"
   instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "test-my-bucket-mahi"
+  object_lock_enabled = true 
 }
